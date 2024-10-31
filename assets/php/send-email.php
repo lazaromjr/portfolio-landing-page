baa6491aa95b85474050c12a3ca7c504ebbc7f25
lazaromjr <lazaromjr@outlook.com>
2024-10-31 18:34:25 -0300

    Fix e-mail sending
    - Include .htaccess file
    - Customize .htaccess on Github Actions
    
    - Include .env file with smtp configurations
    - Add the Dotenv php package
    - Load .env variables on php to setup smtp
    - Append username e password on deploy, using Github Secrets

--- assets/php/send-email.php
+++ assets/php/send-email.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dotenv\Dotenv;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -14,29 +15,34 @@ error_reporting(E_ALL);
 */
 
 if($_POST){
-
-    $mail = new PHPMailer(true);
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-    // Sets up PHP Mailer
-    $mail->isSMTP();
-    $mail->Host = 'mail.smtp2go.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = getenv('smtp_username');
-    $mail->Password = getenv('smtp_password');;
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 2525;
-
-    $mail->setFrom('contact@lazaromonteiro.com', 'Portfolio Webpage');
-    $mail->addAddress('contact@lazaromonteiro.com', 'Lazaro Monteiro');
-
-    $mail->isHTML(false);
-    $mail->Subject = 'Contact from lazaromonteiro.com webpage';
-    $mail->Body = 'From: ' . $_POST['email'] . "\nName: " . $_POST['name'] . "\n\n" . $_POST['message'];
-
     try {
+
+        // Load .env
+        $dotenv = Dotenv::createImmutable(__DIR__, '../../smtp.env');
+        $dotenv->safeLoad();
+
+        $mail = new PHPMailer(true);
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+        // Sets up PHP Mailer
+        $mail->isSMTP();
+        $mail->Host = $_ENV['HOST'];
+        $mail->SMTPAuth = $_ENV['AUTH'];
+        $mail->Username = $_ENV['USERNAME'];
+        $mail->Password =  $_ENV['PASSWORD'];
+        $mail->SMTPSecure = $_ENV['SECURE'];
+        $mail->Port = $_ENV['PORT'];
+
+        $mail->setFrom($_ENV['FROM'], 'Portfolio Webpage');
+        $mail->addAddress('contact@lazaromonteiro.com', 'Lazaro Monteiro');
+
+        $mail->isHTML(false);
+        $mail->Subject = 'Contact from lazaromonteiro.com webpage';
+        $mail->Body = 'From: ' . $_POST['email'] . "\nName: " . $_POST['name'] . "\n\n" . $_POST['message'];
+    
         $mail->send();
         echo "<p style='color:green'> The message was successfully sent! </p>";
+    
     }
     catch (Exception $e) {
         echo "<script> console.error(" . $e . ")</script>";
