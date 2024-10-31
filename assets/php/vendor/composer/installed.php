3badd9855a7897c1164fe6bb2526ee45bdd0f010
lazaromjr <lazaromjr@outlook.com>
2024-10-31 13:08:39 -0300

    Add php dependencies
    - Add php composer along with PhpMailer package
    - These assets allow to send e-mails through the contact form

---
+++ assets/php/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '85eaf00f84483e725f1b86c10305d00cb3f988f9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '85eaf00f84483e725f1b86c10305d00cb3f988f9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php-console/php-console' => array(
+            'pretty_version' => '3.1.8',
+            'version' => '3.1.8.0',
+            'reference' => 'aa1d71d4ea3dc91e126edc9aa4f3c10eb8559cff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-console/php-console',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.2',
+            'version' => '6.9.2.0',
+            'reference' => 'a7b17b42fa4887c92146243f3d2f4ccb962af17c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
